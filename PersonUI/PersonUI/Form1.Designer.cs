b0e839f27a8f7c8ddcce936b38bde80b2d2c6e91
mominrashid <momin0444@gmail.com>
2014-09-27 14:13:12 +0600

    ok.

---
+++ PersonUI/PersonUI/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace PersonUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.firstnameTextBox = new System.Windows.Forms.TextBox();
+            this.reversenameTextBox = new System.Windows.Forms.TextBox();
+            this.fullnameTextBox = new System.Windows.Forms.TextBox();
+            this.lastnameTextBox = new System.Windows.Forms.TextBox();
+            this.middnameTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(310, 300);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(38, 31);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(57, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Middel name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Full Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Reverse Name";
+            // 
+            // firstnameTextBox
+            // 
+            this.firstnameTextBox.Location = new System.Drawing.Point(173, 31);
+            this.firstnameTextBox.Name = "firstnameTextBox";
+            this.firstnameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstnameTextBox.TabIndex = 6;
+            // 
+            // reversenameTextBox
+            // 
+            this.reversenameTextBox.Location = new System.Drawing.Point(173, 229);
+            this.reversenameTextBox.Name = "reversenameTextBox";
+            this.reversenameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.reversenameTextBox.TabIndex = 7;
+            // 
+            // fullnameTextBox
+            // 
+            this.fullnameTextBox.Location = new System.Drawing.Point(173, 169);
+            this.fullnameTextBox.Name = "fullnameTextBox";
+            this.fullnameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fullnameTextBox.TabIndex = 8;
+            // 
+            // lastnameTextBox
+            // 
+            this.lastnameTextBox.Location = new System.Drawing.Point(173, 126);
+            this.lastnameTextBox.Name = "lastnameTextBox";
+            this.lastnameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastnameTextBox.TabIndex = 9;
+            // 
+            // middnameTextBox
+            // 
+            this.middnameTextBox.Location = new System.Drawing.Point(173, 82);
+            this.middnameTextBox.Name = "middnameTextBox";
+            this.middnameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.middnameTextBox.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 431);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.middnameTextBox);
+            this.Controls.Add(this.lastnameTextBox);
+            this.Controls.Add(this.fullnameTextBox);
+            this.Controls.Add(this.reversenameTextBox);
+            this.Controls.Add(this.firstnameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.saveButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox firstnameTextBox;
+        private System.Windows.Forms.TextBox reversenameTextBox;
+        private System.Windows.Forms.TextBox fullnameTextBox;
+        private System.Windows.Forms.TextBox lastnameTextBox;
+        private System.Windows.Forms.TextBox middnameTextBox;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ PersonUI/PersonUI/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonUI
+{
+    internal class Person
+    {
+        public string fname;
+        public string mName;
+        public string lname;
+
+        public string GetFullName()
+        {
+            return fname + "  " + mName + " " + lname;
+
+
+        }
+
+        public string GetreverseFullName()
+        {
+
+            string Fullname = GetFullName();
+            char[] cArray = Fullname.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+
+        }
+    }
+}
